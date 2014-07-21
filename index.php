66033fe656c29206aa0c54bb883763b3f201b974
jordyjes <isc.jordyjes@gmail.com>
2014-07-21 16:40:44 -0500

    fin primera clase
    mejroando.la

--- index.php
+++ index.php
@@ -1,10 +1,84 @@
-<html>
+<!DOCTYPE html> <!--esta linea de codigo indica al navegador que àpartir de aquì utilizamos HTML5-->
+<html lang="es">
+<head>
+	<meta charset="utf-8"/> <!--para indicar la codificacion al navegador-->
+	<meta description"proyecto de la primera clase del curso de diseño web"/>
+	<!--la etiqueta meta keywords ni meta tagas ya no se usa-->
+	<title> PULS 2014 Comunidad de personas atractivas, inteligentes y sexy</title>
+</head>
 <body>
-	<head></head>
-	<section>
-		<article>
-			<h1>hola mundo </h1>
+	<header>
+		<figure id="logo">
+			<img src="logo.png">
+		</figure>
+		<h1>
+			Puls2014: Comunidad de personas atractivas y sexy
+		</h1>
+		<figure id="avatar">
+			<img src="avatar.jpg">
+		</figure>
+	</header>
+	<nav>
+		<ul>
+			<li><a href="#">Python</a></li>
+			<li><a href="#">HTML5</a></li>
+			<li><a href="#">Javascript</a></li>
+			<li><a href="#">CSS·</a></li>
+			<li><a href="#">Django</a></li>
+			<li id="publicar_nav">
+				<a href="#">Publicar</a>
+			</li>
+		</ul>
+	</nav>
+	<section id="contenido">
+		<article class="item">
+			<figure class="imagen_item">
+				<img src="image.jpg">
+			</figure>
+			<h2 class="titulo_item">
+				<a href="#">
+					Título del articulo
+				</a>
+			</h2>
+			<p class="autor_item">
+				por <a href="#">Jordyjes</a>
+			</p>
+			<div class="datos_item">
+			<a href="#" class="tag_item">pony</a>
+			<span class="fecha_item">
+				hace<strong>5</strong>min
+			</span>	
+			<a href="#" class="comentarios_item">69</a>
+			<a href="#" class="guardar_item"></a>
+			</div>
+			<div class="votacion">
+				<a href="#" class="up"></a>
+				728
+				<a href="#" class="down"></a>
+			</div>
 		</article>
 	</section>
+</header>
+<footer>
+	<p>
+	<strong>Power by Jordyjes</strong>
+	</p>
+	<p>
+		mejorando.la 2014
+	</p>
+</footer>
+
 </body>
 </html>
+
+<!-- NOTAS
+en la web para trabjar con idiomas se debe hacer asi:
+sitio.com/es y sitio.com/en, esto esta mal.
+en.sitio.com y es.sitio.com o sitio.com.es esta bien.
+........................
+
+establecer codificacion utf-8
+
+la diferencia eentre id y class, id es irrepetible es unico
+y class es repetible y duplicable en multiples elementos.
+-->
